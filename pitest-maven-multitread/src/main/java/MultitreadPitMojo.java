0ae716824d68c8a1d9f5554816c6e6aec293f8d8
Michał Mnich <michael.mnich@gmail.com>
2016-09-11 19:46:33 +0200

    Gotowy dzialajacy wlasny goal do mavena dlpa pita plus nowy projekt do doktoratu

--- pitest-maven-multitread/src/main/java/MultitreadPitMojo.java
+++ pitest-maven-multitread/src/main/java/MultitreadPitMojo.java
@@ -1,14 +1,29 @@
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.pitest.functional.Option;
+import org.pitest.maven.AbstractPitMojo;
+import org.pitest.maven.MojoToReportOptionsConverter;
+import org.pitest.maven.SurefireConfigConverter;
+import org.pitest.mutationtest.config.ReportOptions;
+import org.pitest.mutationtest.tooling.CombinedStatistics;
+
 /**
  * Created by gosc on 10.09.2016.
  */
-@Mojo(name = "dupa")
-class MultitreadPitMojo extends AbstractMojo {
-    @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        System.out.println("NO zesz dupa dupa dupa");
+@Mojo(name = "dupa", defaultPhase = LifecyclePhase.VERIFY, requiresDependencyResolution = ResolutionScope.TEST)
+class MultitreadPitMojo extends AbstractPitMojo {
+
+
+    protected Option<CombinedStatistics> AnalyseAndMutateProject() throws MojoExecutionException {
+
+
+        final ReportOptions data = new MojoToReportOptionsConverter(this, new SurefireConfigConverter(), this.filter).convert();
+
+
+        return Option.some(this.goalStrategy.execute(detectBaseDir(), data, this.plugins, getEnvironmentVariables()));
     }
+
+
 }
