7df1ed3cf85305b1b0fe54faf1e2d84aaa5a5ffe
Michał Mnich <michael.mnich@gmail.com>
2016-09-13 01:02:48 +0200

    szablon drzewiastej struktury odpalania pitó

--- pitest-maven-multitread/src/main/java/MultitreadPitMojo.java
+++ pitest-maven-multitread/src/main/java/MultitreadPitMojo.java
@@ -14,15 +14,28 @@ import org.pitest.mutationtest.tooling.CombinedStatistics;
  */
 @Mojo(name = "dupa", defaultPhase = LifecyclePhase.VERIFY, requiresDependencyResolution = ResolutionScope.TEST)
 class MultitreadPitMojo extends AbstractPitMojo {
-
+    EncapsulatedPit root;
 
     protected Option<CombinedStatistics> AnalyseAndMutateProject() throws MojoExecutionException {
 
+
+        root = new EncapsulatedPit();
+
+        //metody get i set targetClasses daja nam mozliwość operowania na danych i mozemy manipulowac lista klass
         final ReportOptions data = new MojoToReportOptionsConverter(this, new SurefireConfigConverter(), this.filter).convert();
 
-        CombinedStatistics ex =this.goalStrategy.execute(detectBaseDir(), data, this.plugins, getEnvironmentVariables());
+        //tutaj bedziemy wstryzkiwac singletownowi konfig;
+        //w wersji bayesowskiej jeden konfig na jedna klasse i bedzie sie uczył ??? moze
 
-        return Option.some(ex);
+        CombinedStatistics ex =root.Goal.execute(detectBaseDir(), data, this.plugins, getEnvironmentVariables());
+        //to wyzej to troszke przerobic niech root dostate dane nie jego goal bedzie mugl sobie podzielic prace
+
+        //po wykonaniu wszytkich instancji pita zbiorczo zebrac dane z naszzehj klasy albo zbiorczo je tam zapisywac upenic sie ze jest unikatowa
+        //synchronizacja () nastpnie po zebraniu danych poprawic konfig zgodnie za bayesem
+
+
+        return Option.some(ex); //zwrotka dla obsługi błedów przeniesc jednak coalosc tu
+
     }
 
 
