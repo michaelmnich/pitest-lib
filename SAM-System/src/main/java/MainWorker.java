9c515ca87592bf2ef6c123a68620b9ebe03f44a1
Michał Mnich <michael.mnich@gmail.com>
2016-10-25 13:51:00 +0200

    Nowy projekt
    Nowy projekt

---
+++ SAM-System/src/main/java/MainWorker.java
@@ -0,0 +1,10 @@
+/**
+ * Created by gosc on 25.10.2016.
+ */
+public class MainWorker {
+    public static void main(final String[] args) {
+
+        System.out.println("DUPA TESTOWA");
+
+        }
+    }
